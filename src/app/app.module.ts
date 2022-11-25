fed7f4cd702da8b403937ce1b2f6987db8171be7
Peter C.Longrigg <plongrigg@gmail.com>
2022-11-25 17:18:35 -0500

    update angular material to 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgxMatSearchboxModule } from '@fgrid-ngx/mat-searchbox';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
